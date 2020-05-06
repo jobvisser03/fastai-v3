d92cc26ed2dc304c07dbcc3d7ff5af89a09cdcf3
jobvisser03 <jobvisserhcm@gmail.com>
2020-05-06 15:03:56 +0200

    added cheese model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/w49a6b022s9viov/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['brie','camembert','comte', 'roquefort']
 path = Path(__file__).parent
 
 app = Starlette()
